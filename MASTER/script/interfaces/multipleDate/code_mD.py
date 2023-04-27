aad1f7a4c91c6c414ca42e6fa97247522732664b
Matteo Grazioso <884055@stud.unive.it>
2023-04-27 11:23:49 +0200

    Update interface multiple date

--- MASTER/script/interfaces/multipleDate/code_mD.py
+++ MASTER/script/interfaces/multipleDate/code_mD.py
@@ -11,34 +11,21 @@ import pandas as pd
 import numpy
 import plotly.io as pio
 
-pio.templates.default = "plotly_white" 
-#pio.templates.default = "plotly_dark" 
+import myfunctions_md as f_md
 
-# Data 
+pio.templates.default = "plotly_white" 
+# pio.templates.default = "plotly_dark" 
+
+# Import the datasets
 directory = '../../../transformData/'
 df = pd.read_csv(directory+'valTimeSlotsHour.csv')
 df_validation = pd.read_csv(directory+'validation.csv')
 
-# Se ci sono delle date comprese tra 'start_date' and 'end_date' che non contengono timbrature non vogliamo 
-# che siano visualizzate quindi andiamo a creare una lista di 'disabled_days' con le date che devono essere 
-# disabilitiate per questo problema 
-start_date = df['date'].min()
-end_date = df['date'].max() 
-start_date = datetime.strptime(start_date, '%Y-%m-%d')
-end_date = datetime.strptime(end_date, '%Y-%m-%d')
-delta = end_date - start_date 
-all_date = []
-for i in range(delta.days + 1):
-    day = start_date + timedelta(days=i)
-    #day.strftime('%Y-%m-%d')
-    day = str(day)[:10]
-    all_date.append(day)
-all_date = set(all_date)  
+# Disable the days that are not in the dataset: disabled_days_md
+disabled_days = f_md.disabled_days_md(df)
 
-date = df['date'].unique()
-date = set(date)
-disabled_days = all_date - date
-disabled_days = list(disabled_days)
+# Print the disabled days
+print("disabled_days: ", disabled_days)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.UNITED, dbc.icons.BOOTSTRAP]
 MASTER_LOGO = "http://www.master-project-h2020.eu/wp-content/uploads/2018/02/cropped-Master-logo-1.png"
@@ -48,19 +35,22 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # make a reuseable navitem for the different examples
 nav_item = dbc.NavItem(dbc.NavLink("Link", href="#"))
 
+# Navbar 
 navbar = dbc.Navbar(
             dbc.Container(
                 [
+                    # Logo
                     dbc.Row([
                         dbc.Col([
                             html.Img(src=MASTER_LOGO, height="40px"),
-                            # dbc.NavbarBrand("MASTER", className="ms-2")
                         ],
-                        width={"size":"auto"})
+                        width={"size":"auto"},
+                        style={'padding-top': '10px', 'padding-bottom': '10px', 'margin-right': '5px'})
                     ],
                     align="center",
                     className="g-3"),
 
+                    # Navbar Toggler
                     dbc.Row([
                         dbc.Col([
                             dbc.Nav([
@@ -92,9 +82,10 @@ navbar = dbc.Navbar(
                         ],
                         width={"size":"auto"})
                     ],
-                    align="center"),
+                    align="center", style={'font-size': '11px'}),
                     dbc.Col(dbc.NavbarToggler(id="navbar-toggler", n_clicks=0)),
                     
+                    # Search bar
                     dbc.Row([
                         dbc.Col(
                              dbc.Collapse(
@@ -113,51 +104,61 @@ navbar = dbc.Navbar(
                 ] ,
             fluid=True
             ),
-    color="secondary",
+    color="#007bff29",
     dark=False,
     style={'padding-top': '10px', 'margin-bottom': '30px', 'border-radius': '10px'}
 )
 
+# Callback to toggle collapse on small screens
 @dash.callback(
     Output("navbar-collapse", "is_open"),
     [Input("navbar-toggler", "n_clicks")],
     [State("navbar-collapse", "is_open")],
 )
+
+# Toggle the collapse and change icon on toggle
 def toggle_navbar_collapse(n, is_open):
     if n:
         return not is_open
     return is_open
 
+# Header of the page
 header = html.Div(children=
         [
         #html.Img(src=MASTER_LOGO, height="30px", style={'float': 'center'}),
         html.H1(children = ['Multiple ASpect TrajEctoRy management and analysis'],
-            style = {'font-family':'Comic Sans MS', 'color':'#3A6BAC','textAlign':'center'}),
+                    style = {'font-family':'Comic Sans MS', 'color':'#3A6BAC','textAlign':'center'}),
         html.H2(children = ['ACTV validation'],
-                style={'font-family':'Comic Sans MS', 'color':'#3A6BAC','textAlign':'center', 
+                    style={'font-family':'Comic Sans MS', 'color':'#3A6BAC','textAlign':'center', 
                        'padding-top': '20px'}),
         html.H3(children = ['Multiple Date Analysis'],
-                style={'font-family':'Comic Sans MS', 'color':'#3A6BAC','textAlign':'center', 
+                    style={'font-family':'Comic Sans MS', 'color':'#3A6BAC','textAlign':'center', 
                        'padding-top': '20px', 'font-weight': 'bold'})
         ])
 
+# Layout of the page 
 app.layout = html.Div([
     html.Div(children=[
         navbar, header,
             
         html.Br(),
-        html.Label('Date'),
+        # html.Label('Date'),
+        html.Label(children = ['Date'],
+                    style={'margin-right': '10px', 'font-weight': 'bold', 'font-size': '12px', 
+                           'padding-top': '15px'}),
         dcc.DatePickerRange(
             id='my-date-picker-range',
             min_date_allowed=df['date'].min(),
             max_date_allowed=df['date'].max(),
             initial_visible_month=df['date'].min(),
+            disabled_days=disabled_days,
             start_date=df['date'].min()#,
-            #end_date=df['date'].max()
+            # end_date=df['date'].max()
         ),
         
         html.Br(),
-        html.Label('Ticket'),
+        html.Label(children = ['Ticket type'],
+                    style={'font-weight': 'bold', 'font-size': '12px', 'padding-top': '15px'}),
         dcc.Dropdown(
             id='my-dynamic-dropdown',
             options=[{'label': '24 hours', 'value': 0},
@@ -169,7 +170,7 @@ app.layout = html.Div([
             placeholder="Select a ticket type",
             style={'width': 400, 'align-items': 'left', 'justify-content': 'left'}
         ),
-    ],style={'padding': 10, 'flex': 1}),
+    ],style={'padding': 10, 'flex': 1, 'background-color': '#F0F0F0'}),
 
     html.Div(children=[
         dcc.Graph(id='mymap'),#,style={'width': '20%','display': 'inline-block'}),#style={'width': '90vh', 'height': '90vh'}),
@@ -178,11 +179,11 @@ app.layout = html.Div([
                                     #'borderStyle': 'dashed',
                                     #'borderRadius': '1px'}),
         dcc.Graph(id='bar-chart',clickData=None)#,style={'width': '20%','display': 'inline-block'})
-    ], style={'padding': 10, 'flex': 1})
+    ], style={'padding': 10, 'flex': 1}),
 
 ], style={'display': 'flex', 'flex-direction': 'row'})
 
-
+# Callback for the map
 @app.callback(
     Output(component_id='mymap', component_property='figure'),
     [Input(component_id='my-date-picker-range', component_property='start_date'),
@@ -190,6 +191,7 @@ app.layout = html.Div([
     Input(component_id='my-dynamic-dropdown', component_property='value')]#, 
 )
 
+# Function to update the map
 def update_output(start_date,end_date,value):
     len_choices = len(value)
     if(len_choices == 0 or start_date > end_date or start_date == end_date) : 
@@ -213,9 +215,9 @@ def update_output(start_date,end_date,value):
         dff.sort_values(by=['stop_id'], inplace=True)
 
     fig = px.scatter_mapbox(dff, lat='lat', lon='lon', color ='counts', size = 'counts', 
-                            mapbox_style="carto-positron", width=1300, height=500, zoom=11.1, 
+                            mapbox_style="carto-positron", width=1300, height=500, zoom=11, 
                             #animation_frame = 'date_time',#animation_group='travel_type',
-                            color_continuous_scale='viridis',
+                            color_continuous_scale='ice',
                             range_color=[dff['counts'].min(),dff['counts'].max()],#size_max=50,
                             center = {'lon': 12.337817, 'lat': 45.44},hover_data={'lat': False, 'lon': False,'name_stop':True,'counts': True,})
     
@@ -225,7 +227,7 @@ def update_output(start_date,end_date,value):
 
     return fig
 
-
+# Callback for the bar chart
 @app.callback(
     Output(component_id='bar-chart', component_property='figure'),
     [Input(component_id='my-date-picker-range', component_property='start_date'),
@@ -233,6 +235,7 @@ def update_output(start_date,end_date,value):
     Input(component_id='my-dynamic-dropdown', component_property='value')]#, 
 )
 
+# Function to update the bar chart
 def update_output_sec(start_date,end_date,value):
 
     len_choices = len(value)
@@ -259,7 +262,7 @@ def update_output_sec(start_date,end_date,value):
     my_time_min = dff2['time'].min()
     my_time_max = dff2['time'].max()
 
-    fig2 = px.bar(dff2,x='time',y='counts',hover_data=['counts'], color='counts',color_continuous_scale='viridis',text_auto='.2s',
+    fig2 = px.bar(dff2,x='time',y='counts',hover_data=['counts'], color='counts',color_continuous_scale='ice',text_auto='.2s',
         height=400,width=1300,labels={'time':'Time slots'})
         #labels={'counts':'Number of tickets'})
     fig2.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
